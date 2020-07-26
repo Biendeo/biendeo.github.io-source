4a993a23ee6b05a931d54dc3b118c55c869247f9
Thomas Moffet <pantherman1996@gmail.com>
2020-07-27 08:46:03 +1000

    Corrected git add.

--- Publish.ps1
+++ Publish.ps1
@@ -2,7 +2,7 @@
 Push-Location
 Set-Location .\public
 if ((& git diff --compact-summary) -ne "") {
-    & git diff -A
+    & git add -A
     & git commit -m "$(& git log -1 --pretty=%B)"
     & git push
 }
